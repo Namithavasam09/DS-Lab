8d299e85a82132e146db4662c96383fde98f7862
Namithavasam09 <93445620+Namithavasam09@users.noreply.github.com>
2021-11-09 12:17:29 +0530

    Update binary_search_NR.c

--- binary_search_NR.c
+++ binary_search_NR.c
@@ -28,4 +28,7 @@ int binary_search(int a[],int n,int ele){
                 return mid;
     }return -1;
 }
-
+ /*Enter the number of elements:5
+    Enter the array elements:10 20 30 40 50
+    Element to be searched:30
+ Element found at 2 postion*/
